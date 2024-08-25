f65f6f24685056ada99a8d39deea4720748cb6a9
SidraQurban <sidraqurban77@gmail.com>
2024-08-25 19:09:37 +0500

    /

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,9 +9,15 @@ import { Fontisto, Feather } from "react-native-vector-icons";
 
 const Header = () => {
   return (
-    <View style={{ top: responsiveHeight(-6), padding: responsiveWidth(4) }}>
+    <View
+      style={{
+        top: responsiveHeight(-3),
+        padding: responsiveWidth(4),
+      }}
+    >
       <View
         style={{
+          marginHorizontal: responsiveWidth(2),
           flexDirection: "row",
           alignItems: "center",
           justifyContent: "space-between",
@@ -25,9 +31,31 @@ const Header = () => {
             resizeMode: "contain",
           }}
         />
-        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-          <Fontisto name="bell" size={20} />
-          <Feather name="shopping-cart" size={20} />
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <View style={{ marginRight: responsiveWidth(6) }}>
+            <Fontisto
+              name="bell"
+              size={25}
+              style={{ top: responsiveHeight(1.75) }}
+            />
+          </View>
+          <View
+            style={{
+              height: responsiveHeight(7),
+              width: responsiveHeight(7),
+              borderRadius: responsiveHeight(1.5),
+              backgroundColor: "#ced4da",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Feather name="shopping-cart" size={25} />
+          </View>
         </View>
       </View>
     </View>
