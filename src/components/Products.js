cbfd50679034c676efb5c1049fb69a03783020fe
SidraQurban <sidraqurban77@gmail.com>
2024-09-02 21:16:27 +0500

    ..

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,9 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { responsiveFontSize } from "react-native-responsive-dimensions";
+import {
+  responsiveFontSize,
+  responsiveHeight,
+} from "react-native-responsive-dimensions";
 import { TouchableOpacity } from "react-native";
 
 const Products = () => {
@@ -17,9 +20,44 @@ const Products = () => {
         </Text>
         <TouchableOpacity>
           <Text style={{ color: "#4ea8de", fontSize: responsiveFontSize(2) }}>
-            Shop for more
+            Shop more
           </Text>
         </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          marginTop: 10,
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <View
+          style={{
+            width: responsiveHeight(25),
+            height: responsiveHeight(27),
+            borderRadius: responsiveHeight(2),
+            backgroundColor: "blue",
+          }}
+        >
+          <View
+            style={{ backgroundColor: "red", height: responsiveHeight(18) }}
+          >
+            <Text>Hi</Text>
+          </View>
+          <View>
+            <Text>Product</Text>
+          </View>
+        </View>
+        <View
+          style={{
+            width: responsiveHeight(22),
+            height: responsiveHeight(29),
+            borderRadius: responsiveHeight(2),
+            backgroundColor: "blue",
+          }}
+        >
+          <Text>Hi</Text>
+        </View>
       </View>
     </View>
   );
