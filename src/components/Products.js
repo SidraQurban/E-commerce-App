08ed8db05f687a696d5789eb39485e17532209ce
SidraQurban <sidraqurban77@gmail.com>
2024-09-02 23:21:08 +0500

    //

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,12 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 import {
   responsiveFontSize,
   responsiveHeight,
+  responsiveWidth,
 } from "react-native-responsive-dimensions";
 import { TouchableOpacity } from "react-native";
+import { AntDesign } from "react-native-vector-icons";
 
 const Products = () => {
   return (
@@ -36,16 +38,56 @@ const Products = () => {
             width: responsiveHeight(25),
             height: responsiveHeight(27),
             borderRadius: responsiveHeight(2),
-            backgroundColor: "blue",
+            backgroundColor: "#f8f9fa",
           }}
         >
-          <View
-            style={{ backgroundColor: "red", height: responsiveHeight(18) }}
-          >
-            <Text>Hi</Text>
-          </View>
+          {/* Image */}
+          <Image
+            source={require("../../assets/blush.png")}
+            style={{
+              backgroundColor: "#e9ecef",
+              height: responsiveHeight(17),
+              resizeMode: "contain",
+              width: "100%",
+            }}
+          />
+          {/* Product Text  */}
           <View>
-            <Text>Product</Text>
+            <Text
+              style={{
+                fontWeight: "bold",
+                fontSize: responsiveFontSize(2),
+                marginTop: responsiveHeight(0.5),
+                marginBottom: responsiveHeight(0.5),
+              }}
+            >
+              Pixi Blush on
+            </Text>
+            {/* Pricing */}
+            <View
+              style={{
+                flexDirection: "row",
+                marginBottom: responsiveHeight(0.5),
+              }}
+            >
+              <Text style={{ color: "#6c757d" }}>Rs.1,764</Text>
+              <Text
+                style={{
+                  marginLeft: responsiveWidth(2),
+                  color: "#6c757d",
+                  height: responsiveHeight(2.4),
+                  backgroundColor: "#ced4da",
+                }}
+              >
+                -50%
+              </Text>
+            </View>
+            {/* Product Rating */}
+            <View style={{ flexDirection: "row" }}>
+              <AntDesign name="star" color="#fcbf49" size={18} />
+              <Text style={{ marginLeft: responsiveWidth(2) }}>4.6(127)</Text>
+              <Text style={{ marginLeft: responsiveWidth(2) }}> 1.1k sold</Text>
+            </View>
           </View>
         </View>
         <View
