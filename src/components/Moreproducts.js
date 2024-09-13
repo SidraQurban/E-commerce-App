17c87c6225026e19bfa10cb2097fe7221e5ed04e
SidraQurban <sidraqurban77@gmail.com>
2024-09-13 20:40:59 +0500

    updated

--- src/components/Moreproducts.js
+++ src/components/Moreproducts.js
@@ -11,9 +11,21 @@ import { productData } from "../Constant";
 import { useNavigation } from "@react-navigation/native";
 
 const Moreproducts = () => {
-  <View>
-    <Text>Hi</Text>
-  </View>;
+  const navigation = useNavigation();
+  const [active, setActive] = useState(null);
+  const handlePress = (id) => {
+    setActive(active === id ? null : id);
+  };
+
+  const rows = [];
+  for (let i = 0; i < productData.length; i += 2) {
+    rows.push(productData.slice(i, i + 2));
+  }
+  return (
+    <View>
+      <Text>Hi</Text>
+    </View>
+  );
 };
 
 export default Moreproducts;
