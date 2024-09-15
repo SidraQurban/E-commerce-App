23979387d4d81e9e0d001510673e144ff5730f57
SidraQurban <sidraqurban77@gmail.com>
2024-09-15 22:49:21 +0500

    ./

--- src/screens/DetailProduct.js
+++ src/screens/DetailProduct.js
@@ -1,10 +1,19 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView, Pressable } from "react-native";
 import React from "react";
+import { FontAwesome } from "@expo/vector-icons";
 
 const DetailProduct = () => {
   return (
     <View>
-      <Text>DetailProduct</Text>
+      <SafeAreaView
+        style={{ flexDirection: "row", marginHorizontal: 16, marginTop: 40 }}
+      >
+        <Pressable style={{ flex: 1 }}>
+          <FontAwesome name={"arrow-circle-left"} size={28} color="white" />
+        </Pressable>
+
+        <FontAwesome name={"heart-o"} size={28} color="white" />
+      </SafeAreaView>
     </View>
   );
 };
