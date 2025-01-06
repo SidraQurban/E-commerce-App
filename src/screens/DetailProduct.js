e9e1927498d03a422532eeeed9721780a1a1befa
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-01-06 07:38:17 -0800

    ./

--- src/screens/DetailProduct.js
+++ src/screens/DetailProduct.js
@@ -1,12 +1,31 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { responsiveHeight } from 'react-native-responsive-dimensions';
 
 const DetailProduct = () => {
   return (
-    <View>
-      <Text>DetailProduct</Text>
-    </View>
-  )
+    <SafeAreaView>
+      <View>
+        <View
+          style={{
+            backgroundColor: "#e9ecef",
+            borderBottomRightRadius: responsiveHeight(5),
+          }}
+        >
+          <Text>DetailProduct</Text>
+          <Image
+            source={require("../../assets/lipstick.png")}
+            style={{
+              resizeMode: "contain",
+              alignSelf: "center",
+              height: responsiveHeight(20),
+            }}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 export default DetailProduct
