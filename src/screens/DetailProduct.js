87b4747b122f4efb46878a98d884f05b580f5637
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-01-06 07:24:00 -0800

    ..

--- src/screens/DetailProduct.js
+++ src/screens/DetailProduct.js
@@ -1,30 +1,12 @@
-import { View, Text, SafeAreaView, Pressable, TouchableOpacity } from "react-native";
-import React from "react";
-import { FontAwesome } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
+import { View, Text } from 'react-native'
+import React from 'react'
 
 const DetailProduct = () => {
-  const navigation = useNavigation();
   return (
     <View>
-      <SafeAreaView
-        
-      >
-        <View style={{flexDirection: "row", marginHorizontal: 16, marginTop: 40, justifyContent:"space-between", alignItems: "center"}}>
-        <TouchableOpacity onPress={()=> navigation.goBack()} >
-          <FontAwesome name={"arrow-circle-left"} size={28} color="white" />
-        </TouchableOpacity>
-        <TouchableOpacity >
-        <FontAwesome name={"heart-o"} size={28} color="black" />
-        </TouchableOpacity>
-
-       
-          
-        </View>
-       
-      </SafeAreaView>
+      <Text>DetailProduct</Text>
     </View>
-  );
-};
+  )
+}
 
-export default DetailProduct;
+export default DetailProduct
