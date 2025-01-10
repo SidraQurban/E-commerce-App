d84598c3834bd58c8c99f4ec6df16008aa4e4830
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-01-10 04:49:33 -0800

    updated

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,11 +1,26 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
+import Header from "../components/Header";
+import Searchbar from "../components/Searchbar";
+import Deals from "../components/Deals";
+import { responsiveWidth } from "react-native-responsive-dimensions";
+import Footer from "../components/Footer";
+import Categories from "../components/Categories";
 
 const Search = () => {
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
+    <>
+    <ScrollView
+      style={{ flex: 1, padding: responsiveWidth(3) }}
+      showsVerticalScrollIndicator={false}
+    >
+      <Searchbar/>
+      <Categories/>
+     
+    </ScrollView>
+    <Footer />
+  </>
+
   );
 };
 
