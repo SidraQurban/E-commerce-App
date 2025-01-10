ad624e35acaf2abab45cde97bd1f8e882db46f28
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-01-10 06:02:02 -0800

    done

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -1,11 +1,39 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { responsiveFontSize, responsiveHeight } from "react-native-responsive-dimensions";
+import { useNavigation } from "@react-navigation/native";
+import {Ionicons} from "react-native-vector-icons";
+import Footer from "../components/Footer";
 
 const Cart = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>Cart</Text>
-    </View>
+    <>
+      <SafeAreaView>
+        <View style={{flexDirection:"row"}}>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Ionicons
+              name="chevron-back"
+              size={25}
+              style={{ marginTop: responsiveHeight(2.5) }}
+            />
+          </TouchableOpacity>
+          <Text
+            style={{
+              fontSize: responsiveFontSize(2.2),
+              fontWeight: "bold",
+              textAlign: "center",
+              marginLeft: responsiveHeight(15),
+              marginTop: responsiveHeight(2),
+            }}
+          >
+            Your Cart
+          </Text>
+        </View>
+      </SafeAreaView>
+      {/* <Footer/> */}
+    </>
   );
 };
 
