98fa8dbb74bf461656c2412cfbdd63a0a3f62bcd
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-01-11 10:19:07 -0800

    ./

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,7 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React, { useState } from 'react'
 import {MaterialCommunityIcons,MaterialIcons} from "react-native-vector-icons"
-import { responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions';
+import { responsiveFontSize, responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions';
 
 const CartItem = () => {
   const [checked, setChecked] = useState(false);
@@ -16,39 +16,52 @@ const CartItem = () => {
           flexDirection: "row",
           // justifyContent: "space-between",
           padding: 10,
-          height:responsiveHeight(22),
+          height: responsiveHeight(22),
           backgroundColor: "#fff",
         }}
       >
-        <TouchableOpacity onPress={handlePress} style={{marginTop:responsiveHeight(7)}}>
+        <TouchableOpacity
+          onPress={handlePress}
+          style={{ marginTop: responsiveHeight(7) }}
+        >
           <MaterialCommunityIcons
             name={checked ? "checkbox-outline" : "checkbox-blank-outline"}
             color={checked ? "green" : "black"}
             size={25}
           />
         </TouchableOpacity>
+        {/* p.Image */}
         <View
           style={{
-            marginTop:responsiveHeight(0.5),
-            height: responsiveHeight(18),
+            marginTop: responsiveHeight(2.5),
+            height: responsiveHeight(15),
             width: responsiveWidth(30),
-            borderRadius:responsiveHeight(1),
+            borderRadius: responsiveHeight(1),
             backgroundColor: "grey",
-            marginLeft:responsiveWidth(5)
+            marginLeft: responsiveWidth(5),
           }}
         >
           <Image
             source={require("../../assets/blush.png")}
             style={{
               resizeMode: "contain",
-              height: responsiveHeight(20),
+              height: responsiveHeight(15),
               width: responsiveWidth(20),
               alignSelf: "center",
             }}
           />
         </View>
-
-        <Text>Blush On</Text>
+        {/* p.name */}
+        <Text
+          style={{
+            marginTop: responsiveHeight(6),
+            marginLeft: responsiveWidth(3),
+            fontSize: responsiveFontSize(2),
+            // fontWeight: "bold",
+          }}
+        >
+          Blush On
+        </Text>
         <TouchableOpacity>
           <MaterialIcons name="delete" size={25} color="red" />
         </TouchableOpacity>
