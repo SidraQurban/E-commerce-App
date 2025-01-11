bf49a820d7f7bee8857e3aaecb94c7b721179990
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-01-11 04:15:17 -0800

    ./

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,6 +1,7 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React, { useState } from 'react'
 import {MaterialCommunityIcons} from "react-native-vector-icons"
+import { responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions';
 
 const CartItem = () => {
   const [checked, setChecked] = useState(false);
@@ -10,14 +11,24 @@ const CartItem = () => {
   
   return (
     <View>
-     <TouchableOpacity onPress={handlePress}>
-      <MaterialCommunityIcons
-       name={checked ? "checkbox-outline" : "checkbox-blank-outline"}
-       color = {checked ? "green" : "black"}
-       size={25}/>
-     </TouchableOpacity>
+      <View style={{flexDirection:"row", justifyContent:"space-between", padding:10, backgroundColor:"#fff"}}>
+        <TouchableOpacity onPress={handlePress}>
+          <MaterialCommunityIcons
+            name={checked ? "checkbox-outline" : "checkbox-blank-outline"}
+            color={checked ? "green" : "black"}
+            size={25}
+          />
+        </TouchableOpacity>
+        <Image
+          source={require("../../assets/blush.png")}
+          style={{ resizeMode: "contain", height: responsiveHeight(20),
+            width: responsiveWidth(10),
+            // marginLeft: responsiveWidth(3)
+           }}
+        />
+      </View>
     </View>
-  )
+  );
 }
 
 export default CartItem
