62bbe638cd9e3eddfebd4b16a615073754eb5afd
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-01-11 04:12:13 -0800

    ../

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,10 +1,21 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import {MaterialCommunityIcons} from "react-native-vector-icons"
 
 const CartItem = () => {
+  const [checked, setChecked] = useState(false);
+  const handlePress = () => {
+    setChecked(!checked);
+  }
+  
   return (
     <View>
-      <Text>CartItem</Text>
+     <TouchableOpacity onPress={handlePress}>
+      <MaterialCommunityIcons
+       name={checked ? "checkbox-outline" : "checkbox-blank-outline"}
+       color = {checked ? "green" : "black"}
+       size={25}/>
+     </TouchableOpacity>
     </View>
   )
 }
