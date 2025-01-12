ec294572687642aaeca202c66b0337a7773fcd8f
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-01-12 10:56:49 -0800

    done

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -75,6 +75,9 @@ const CartItem = () => {
         <Text>
         Rs. 1,490
         </Text>
+        <TouchableOpacity>
+          <MaterialIcons name="delete" size={25} color="red" />
+        </TouchableOpacity>
       </View>
     </View>
   );
