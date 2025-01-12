b1d8ad5ffa2e365e78bcba573866a3470f12b030
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-01-12 10:59:31 -0800

    ./

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -75,6 +75,14 @@ const CartItem = () => {
         <Text>
         Rs. 1,490
         </Text>
+         <View
+                    style={{
+                      marginTop: 20,
+                      height: 1,
+                      width: "100%",
+                      backgroundColor: "#ced4da",
+                    }}
+                  />
       </View>
     </View>
   );
