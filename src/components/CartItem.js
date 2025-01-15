f9211aa0b265ace47d2771780ef9cd581e47cbee
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-01-15 05:11:11 -0800

    /

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -75,7 +75,7 @@ const CartItem = () => {
         <Text>Rs. 1,490</Text>
         <Text>helllo</Text>
         <Text>helllo</Text>
-        <MaterialIcons name="delete" size= {20} color="red" />
+        <MaterialIcons name="delete" size= {20} color="blue" style={{height:responsiveHeight(2), width:responsiveWidth(3)}} />
         <Text>Hi</Text>
       </View>
       
