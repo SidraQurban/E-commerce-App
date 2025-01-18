d24791e177085d7c283299f6459627b213621615
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-01-18 10:17:39 -0800

    ../

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -77,6 +77,9 @@ const CartItem = () => {
       </View>
       <View>
         <Text>Hi</Text>
+        <TouchableOpacity>
+          <MaterialCommunityIcons name="hoem" />
+        </TouchableOpacity>
       </View>
     </View>
   );
