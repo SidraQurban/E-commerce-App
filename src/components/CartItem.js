0b39c75554f3db0522981ac4453871c9f7a9986f
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-01-20 06:34:14 -0800

    ./

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -79,7 +79,7 @@ const CartItem = () => {
           <MaterialCommunityIcons
             name="heart-outline"
             size={25}
-            color="#f26a8d"
+            color="black"
             // style={{ marginLeft: responsiveWidth(1.5), marginTop: responsiveHeight(2)}}
           />
         </TouchableOpacity>
