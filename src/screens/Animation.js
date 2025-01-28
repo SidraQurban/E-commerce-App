6d0367695be56bc0ea0c120b0016ba8036f9f527
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-01-27 20:46:41 -0800

    ../

--- src/screens/Animation.js
+++ src/screens/Animation.js
@@ -3,17 +3,20 @@ import React from 'react'
 import LottieView from 'lottie-react-native';
 import { responsiveFontSize, responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions';
 import { useNavigation } from '@react-navigation/native';
-import { Ionicons } from 'react-native-vector-icons';
+import { Entypo } from 'react-native-vector-icons';
 
 
 const Animation = () => {
    const navigation = useNavigation();
   return (
     <View>
-      
-       <TouchableOpacity onPress={() => navigation.goBack()}>
-                  <Ionicons name="chevron-back" size={25} style={{ marginTop: responsiveHeight(2.5) }} />
-                </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        <Entypo
+          name="cross"
+          size={25}
+          style={{ marginTop: responsiveHeight(3), marginLeft: responsiveWidth(90)}}
+        />
+      </TouchableOpacity>
       <LottieView
         style={{ height: responsiveHeight(80), width: responsiveWidth(100) }}
         source={require("../../Animation - 1738038020472.json")}
@@ -25,11 +28,11 @@ const Animation = () => {
           fontSize: responsiveFontSize(2.2),
           alignSelf: "center",
           fontWeight: "bold",
-          marginBottom:responsiveHeight(8),
-          color:"green",
+          marginBottom: responsiveHeight(8),
+          color: "green",
         }}
       >
-       Your order has been placed successfully! 😊
+        Your order has been placed successfully! 😊
       </Text>
     </View>
   );
