e3e50706905ac7718773bc34876d2fdbfc03a952
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-01-28 23:16:12 -0800

    updated

--- src/Constant.js
+++ src/Constant.js
@@ -1,16 +1,22 @@
 export const images = [
   {
     id: 1,
-    img: require("./../assets/image1.png"),
+    
+    img: require("./../assets/img1.jpg"),
   },
   {
     id: 2,
-    img: require("./../assets/image3.png"),
+    img: require("./../assets/img2.webp"),
   },
   {
     id: 3,
-    img: require("./../assets/image2.png"),
+   
+    img: require("./../assets/img3.jpg"),
   },
+  {
+    id: 4,
+    img: require("./../assets/img4.webp"),
+  }
 ];
 
 export const brandsLogo = [
