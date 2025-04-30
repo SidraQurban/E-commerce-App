a42dd704918ac7d3805eddeb2d46ff56d3b6a17b
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-04-29 22:49:19 -0700

    ./

--- src/screens/DetailProduct.js
+++ src/screens/DetailProduct.js
@@ -54,7 +54,7 @@ const DetailProduct = () => {
               <FontAwesome5 name="share" size={23} />
             </TouchableOpacity>
           </View>
-          <View>
+          <View style={{ flexDirection: "row",justifyContent:"space-between" }}>
             {/* price */}
             <Text
               style={{
@@ -69,24 +69,7 @@ const DetailProduct = () => {
             </Text>
           </View>
           {/* rating */}
-          <View
-            style={{
-              flexDirection: "row",
-              marginLeft: responsiveWidth(2.5),
-              marginTop: responsiveHeight(-4),
-            }}
-          >
-            <AntDesign name="star" size={20} color="#fcbf49" />
-            <Text
-              style={{
-                fontSize: responsiveFontSize(2),
-                marginLeft: responsiveWidth(2),
-                color: "#ff6d00",
-              }}
-            >
-              {product.rating} {product.sold}
-            </Text>
-          </View>
+          
           {/* Details */}
           <View
             style={{
