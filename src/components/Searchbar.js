6f523ca6ba74f871a8659305a8532bc8e57fa328
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-05-01 00:03:09 -0700

    updated

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -14,7 +14,6 @@ const camera = async() =>{
     mediaType:"photo",
     maxWidth: responsiveHeight(90),
     maxHeight: responsiveHeight(90),
-
   }
   let isCameraPermitted = await requestCameraPermissions();
   if(isCameraPermitted || Platform.Version >13){
