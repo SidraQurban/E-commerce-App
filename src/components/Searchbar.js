badaf3c299b6e3f0f034cfe457de3932d4b852c1
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-05-02 00:29:58 -0700

    ./

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -24,6 +24,7 @@ const camera = async() =>{
         console.log(response?.assets)
         setImage(response?.assets)
         console.log("Camera permissions granted:", isCameraPermitted);
+        
 console.log("Camera response:", response);
 console.log("Image assets:", response?.assets);
       }
