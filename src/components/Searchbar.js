5bca62bbfac2178c4d0a3c4b106c5cae8cfc831a
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-05-01 23:40:30 -0700

    ./

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -20,7 +20,7 @@ const camera = async() =>{
     launchCamera(options, (response) => { 
       if(response.errorCode){
         alert(response.errorMessage)
-      } else if(
+      } else if
 
       } else {
         console.log(response?.assets)       
