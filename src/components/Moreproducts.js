7337c6d3d6ff9721e5397c5aaeb6b3bce5ff0619
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-05-03 21:59:54 -0700

    ./

--- src/components/Moreproducts.js
+++ src/components/Moreproducts.js
@@ -21,8 +21,9 @@ const Moreproducts = () => {
   for (let i = 0; i < moreProductData.length; i += 2) {
     rows.push(moreProductData.slice(i, i + 2));
   }
+  
   return (
-    
+
     <View>
       <View
         style={{
