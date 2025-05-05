79303e0537cc9ef56950a3bbff3b369f40f60bd6
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-05-04 21:05:29 -0700

    ./

--- src/components/Moreproducts.js
+++ src/components/Moreproducts.js
@@ -16,14 +16,13 @@ const Moreproducts = () => {
   const handlePress = (id) => {
     setActive(active === id ? null : id);
   };
-
   const rows = [];
   for (let i = 0; i < moreProductData.length; i += 2) {
     rows.push(moreProductData.slice(i, i + 2));
   }
 
   return (  
-    
+
     <View>
       <View
         style={{
