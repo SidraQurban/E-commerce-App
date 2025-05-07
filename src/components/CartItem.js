78b8c614de4273b1bc396367889cd9b02b1e32bc
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-05-06 21:55:07 -0700

    ./

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -58,7 +58,7 @@ const CartItem = ({product}) => {
               />
             </View>
             {/* Product Name */}
-            <View>
+            <View style={{flexDirection:"row", justifyContent:"space-between"}}>
             <Text
               style={{
                 marginTop: responsiveHeight(3.6),
