4dfcfab4beab8afb0e4ece86848665c41a0460e5
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-05-07 20:18:11 -0700

    ./

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -72,7 +72,7 @@ const CartItem = ({product}) => {
                   fontSize: responsiveFontSize(2),
                   fontWeight: "bold",
                 }}
-              >        
+              >       
 
                 {product.name}
               </Text>
