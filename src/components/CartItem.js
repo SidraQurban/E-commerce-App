cc104c8aeb69584ea61eab3173d8b51102bb1370
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-05-07 20:23:27 -0700

    ./

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -73,7 +73,7 @@ const CartItem = ({product}) => {
                   fontWeight: "bold",
                 }}
               >
-              setIsVisible(false) // Hide
+              setIsVisible(false) 
                 {product.name}
               </Text>
               {/* Cancel Button */}
