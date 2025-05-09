34b00c02fbc92b0c9ecd6f326442facfe5547a36
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-05-08 22:30:25 -0700

    updated

--- src/Constant.js
+++ src/Constant.js
@@ -139,7 +139,7 @@ export const moreProductData = [
   {
     id: 5,
     img: require("./../assets/skirt.png"),
-    name: "Latest Trendycasual sneaker",
+    name: "Skirt",
     price: "Rs. 2,490",
     discount: "-25%",
     rating: "4.5(230)",
