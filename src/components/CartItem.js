3a5eb829fdd40dc79a1f43ed6fe4169b85cea5b5
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-05-08 22:27:53 -0700

    ./

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -75,7 +75,7 @@ const CartItem = ({product}) => {
               </Text>
               {/* Cancel Button */}
               <TouchableOpacity
-              style={{marginRight: responsiveWidth(5)}}
+              style={{marginLeft: responsiveWidth(5)}}
                 onPress={handleCancel} // Hide component on press
               >
                 <Entypo name="cross" size={20} color="black" />
