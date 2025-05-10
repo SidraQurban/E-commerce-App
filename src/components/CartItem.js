30b1ae09fe96a0e0287e9e8d471b049961ecb2c9
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-05-09 23:08:41 -0700

    ./

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -82,7 +82,7 @@ const CartItem = ({product}) => {
               </TouchableOpacity>
             </View>
           </View>
-    
+                    
           {/* Price */}
           <View>
             <Text
