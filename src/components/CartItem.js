dff93dab2cb2e600fc851213631ee4c3058fd5f2
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-05-14 22:08:49 -0700

    ./

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -70,7 +70,7 @@ const CartItem = ({product}) => {
                   fontSize: responsiveFontSize(2),
                   fontWeight: "bold",
                 }}
- >
+>
               {product.name}
               </Text>
               {/* Cancel Button */}
