2cad19a6cdd35c68ae8fdad2e4812c395571d727
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-05-16 22:17:05 -0700

    ./

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -78,7 +78,7 @@ const CartItem = ({product}) => {
               onPress={() => handleCancel()} // Hide component on press
               >
                          
-              <Text>hello, my nme is sidra</Text>
+              <Text>hello, my nme is</Text>
               <Entypo name="cross" size={20} color="black" />
               </TouchableOpacity>
             </View>
