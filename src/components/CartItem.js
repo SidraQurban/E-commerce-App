62fb9cdf2725273d80dfd2ce582ab318f0be361e
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-05-16 22:16:07 -0700

    ./

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -77,7 +77,8 @@ const CartItem = ({product}) => {
               style={{marginLeft: responsiveWidth(7)}}
               onPress={() => handleCancel()} // Hide component on press
               >
-                
+                         
+                         
               <Entypo name="cross" size={20} color="black" />
               </TouchableOpacity>
             </View>
