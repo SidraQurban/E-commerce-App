0157b14da2bf10ec3fdaa25a5383d98063a98594
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-05-25 23:37:42 -0700

    ./

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -80,7 +80,7 @@ const CartItem = ({product}) => {
              <Entypo name="cross" size={20} color="black" />
               </TouchableOpacity>
 
-\end
+\en
             </View>
           </View>          
           {/* Price */}
