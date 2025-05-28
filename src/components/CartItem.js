dc23e2a829afc0ce90ffc96b10cf82861df17419
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-05-27 21:10:47 -0700

    ./

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -82,7 +82,7 @@ const CartItem = ({product}) => {
             </View>
                    
 
-            {/* Product  
+            {/*   
           </View>          
           {/* Price */}
           <View>
