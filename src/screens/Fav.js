e77de8af77c5c6bd4541e95f10ccba45605fda77
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-05-28 19:46:35 -0700

    updated

--- src/screens/Fav.js
+++ src/screens/Fav.js
@@ -9,39 +9,57 @@ import { responsiveFontSize, responsiveHeight } from 'react-native-responsive-di
 const Fav = () => {
   const navigation = useNavigation();
   return (
-    <View style={{ flex: 1, justifyContent: 'space-between' }}>
+    <View>
       <View style={{ flexDirection: "row" }}>
-                <TouchableOpacity onPress={() => navigation.goBack()}>
-                  <Ionicons
-                    name="chevron-back"
-                    size={25}
-                    style={{ marginTop: responsiveHeight(0.5) }}
-                  />
-                </TouchableOpacity>
-                <Text
-                  style={{
-                    fontSize: responsiveFontSize(2.2),
-                    fontWeight: "bold",
-                    textAlign: "center",
-                    marginLeft: responsiveHeight(15),
-                    marginTop: responsiveHeight(0.5),
-                  }}
-                >
-                  Your Cart
-                </Text>
-              </View>
-              {/* horizontal line */}
-              <View
-                style={{
-                  height: 1,
-                  width: "100%",
-                  backgroundColor: "#e0e1dd",
-                  marginTop: responsiveHeight(2),
-                }}
-              />
-      
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Ionicons
+            name="chevron-back"
+            size={25}
+            style={{ marginTop: responsiveHeight(0.5) }}
+          />
+        </TouchableOpacity>
+        <Text
+          style={{
+            fontSize: responsiveFontSize(2.2),
+            fontWeight: "bold",
+            textAlign: "center",
+            marginLeft: responsiveHeight(15),
+            marginTop: responsiveHeight(0.5),
+          }}
+        >
+          Favorites
+        </Text>
+      </View>
+      {/* horizontal line */}
+      <View
+        style={{
+          height: 1,
+          width: "100%",
+          backgroundColor: "#e0e1dd",
+          marginTop: responsiveHeight(2),
+        }}
+      />
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          height: responsiveHeight(55),
+        }}
+      >
+        <Text
+          style={{
+            fontSize: responsiveFontSize(2.5),
+            fontWeight: "bold",
+            color: "gray",
+          }}
+        >
+          Your favorite items will appear here.
+        </Text>
+      </View>
 
-      <Footer />
+      <View style={{ marginTop: responsiveHeight(30) }}>
+        <Footer />
+      </View>
     </View>
   );
 };
