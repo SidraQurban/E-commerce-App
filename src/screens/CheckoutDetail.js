0a84d74a906d0b441357091784dfbde78a8feed8
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-05-28 17:52:28 -0700

    ....../

--- src/screens/CheckoutDetail.js
+++ src/screens/CheckoutDetail.js
@@ -42,7 +42,7 @@ const CheckoutDetail = () => {
                       />
               {/* <CheckoutForm /> */}
               {/* <PaymentMethod /> */}
-              <Footer />
+              {/* <Footer /> */}
     </SafeAreaView>
    
     </>
