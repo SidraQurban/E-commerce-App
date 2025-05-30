a155eb0df5e4fc752a527a76d78f0fd4a9bf7665
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-05-29 22:19:52 -0700

    updated

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -41,8 +41,8 @@ const Brands = () => {
                   height: responsiveHeight(11),
                   width: responsiveHeight(11),
                   borderRadius: responsiveHeight(5.5),
-                  resizeMode: "cover",
-                }}
+                  resizeMode: "cover",       
+                    }}
               />
   
             </TouchableOpacity>
