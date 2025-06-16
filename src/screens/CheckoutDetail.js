fbdce8f3e8c87d4e64d82a0e5f9d08f06107e3cb
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-06-15 19:31:50 -0700

    ./

--- src/screens/CheckoutDetail.js
+++ src/screens/CheckoutDetail.js
@@ -76,8 +76,9 @@ const CheckoutDetail = () => {
             </Text>
           </View>
         </View>
-        
+
         <CheckoutForm />
+        
         <PaymentMethod />
       </ScrollView>
     </SafeAreaView>
