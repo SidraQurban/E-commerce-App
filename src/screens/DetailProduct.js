24862695f17eb7a1c4978231318a4f93ee7f6ba2
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-06-17 22:41:44 -0700

    ./

--- src/screens/DetailProduct.js
+++ src/screens/DetailProduct.js
@@ -8,7 +8,7 @@ import {useNavigation, useRoute} from '@react-navigation/native';
 const DetailProduct = () => {
 const navigation = useNavigation();
 const route = useRoute();  // Get the passed product data
-  const { product } = route.params;
+const { product } = route.params;
   const handleAddToCart = () =>{
     navigation.navigate("Cart", { product})
   }  
