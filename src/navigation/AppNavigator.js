f5de58f1bbb5fb4f0debf7c0afb26a08a3f70f7d
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-06-17 22:37:43 -0700

    ./

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -14,6 +14,7 @@ import CheckoutDetail from "../screens/CheckoutDetail";
 const Stack = createNativeStackNavigator();
 const AppNavigator = () => {
   return (
+    
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={Home} />
