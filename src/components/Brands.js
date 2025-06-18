180344a42240c06e378dc52bfa4462d7e2f8ccde
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-06-17 22:38:11 -0700

    ./

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -5,7 +5,6 @@ import {
   responsiveHeight,
 } from "react-native-responsive-dimensions";
 import { brandsLogo } from "../Constant";
-
 const Brands = () => {
   const handlePress = (url) => {
     Linking.openURL(url).catch((err) => {
