d322e1ddb511b29e8837d2019c6b4fd59faa52d7
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-06-17 22:41:29 -0700

    ./

--- src/screens/DetailProduct.js
+++ src/screens/DetailProduct.js
@@ -6,7 +6,7 @@ import {Ionicons,AntDesign,FontAwesome,FontAwesome5} from "react-native-vector-i
 import {useNavigation, useRoute} from '@react-navigation/native';
 
 const DetailProduct = () => {
-  const navigation = useNavigation();
+const navigation = useNavigation();
   const route = useRoute();  // Get the passed product data
   const { product } = route.params;
   const handleAddToCart = () =>{
