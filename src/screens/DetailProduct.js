803680814fa82fe52995ffdd37f65a4a28f91574
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-06-18 22:31:51 -0700

    ./

--- src/screens/DetailProduct.js
+++ src/screens/DetailProduct.js
@@ -12,7 +12,6 @@ const { product } = route.params;
 const handleAddToCart = () =>{
   navigation.navigate("Cart", { product})
   }  
-
  const shareProduct = () => {
     Share.share({
       message: `Check out this product: ${product.name} in ${product.price}`,
