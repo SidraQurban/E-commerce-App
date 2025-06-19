03fe4d1cfd40a07f7220014ba15e2de5e9af761a
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-06-19 15:01:33 -0700

    ./

--- src/screens/Animation.js
+++ src/screens/Animation.js
@@ -4,7 +4,6 @@ import LottieView from 'lottie-react-native';
 import { responsiveFontSize, responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions';
 import { useNavigation } from '@react-navigation/native';
 import { Entypo } from 'react-native-vector-icons';
-
 const Animation = () => {
    const navigation = useNavigation();
   return (
