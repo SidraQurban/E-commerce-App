0df1d68daccc3eda6aed6afe93b6bc3901e3a006
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2025-08-24 11:45:03 -0700

    ./

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -20,7 +20,7 @@ const Brands = () => {
           Top Rated Brands
         </Text>
       </View>
-      <Text></Text
+      <Text></Tex
       <View
         style={{
           marginTop: responsiveHeight(1),
